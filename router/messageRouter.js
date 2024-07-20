6788a2aac2aa358c5ed01f238d7cf0a603758e0b
Ar273404 <arun273404@gmail.com>
2024-07-20 18:32:56 +0530

    first commit

---
+++ router/messageRouter.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {
+  sendMessage,
+} from "../controller/messageController.js";
+
+const router = express.Router();
+
+router.post("/send", sendMessage);
+
+
+export default router;
