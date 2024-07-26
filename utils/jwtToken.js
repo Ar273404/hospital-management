4d35d4993de8adfa9e05a6f1db3a28e45953f6d2
Ar273404 <arun273404@gmail.com>
2024-07-26 22:02:39 +0530

    final

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,15 +1,18 @@
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
-  // Determine the cookie name based on the user's role
   const cookieName = user.role === "Admin" ? "adminToken" : "patientToken";
+
+  // Convert COOKIE_EXPIRE from days to milliseconds
+  const cookieExpireDays = parseInt(process.env.COOKIE_EXPIRE, 10);
+  const cookieExpireMs = cookieExpireDays * 24 * 60 * 60 * 1000;
 
   res
     .status(statusCode)
     .cookie(cookieName, token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
+      expires: new Date(Date.now() + cookieExpireMs),
       httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // Only set secure flag in production
+      sameSite: "None", // Adjust based on your needs
     })
     .json({
       success: true,
